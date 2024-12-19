3648b48b6a64f1261ccd4693bfd2f4874633961d
Pr0ximah <51467731+Pr0ximah@users.noreply.github.com>
2024-12-19 19:50:42 +0800

    [feat] UNet train start

--- scripts/train_unet.py
+++ scripts/train_unet.py
@@ -1,5 +1,6 @@
 import sys
-sys.path.append('./')
+
+sys.path.append("./")
 from models.unet import UNet
 import torch
 from torch.utils.data import DataLoader
@@ -8,11 +9,13 @@ from torch.optim.adam import Adam
 from tqdm import trange
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # parameters
-    lr = 5e-5
-    epochs = 100
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    lr = 1e-3
+    epochs = 1000
+    load_pretrained = False
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f" ** Using device: {device}")
 
     dataset = DRIVE_Dataset()
@@ -21,10 +24,18 @@ if __name__ == '__main__':
     model = UNet(3)
     optimizer = Adam(model.parameters(), lr=lr)
     loss = torch.nn.MSELoss()
-    model.to(device)
+    model = model.to(device)
+
+    if load_pretrained:
+        pretrained_model_dir = "models/logs/unet_model_500.pth"
+        model.load_state_dict(torch.load(pretrained_model_dir, weights_only=False))
+        init_epoch = int(pretrained_model_dir.split("_")[-1].split(".")[0])
+        print(
+            f" ** Loaded model from {pretrained_model_dir}, init epoch {init_epoch + 1}"
+        )
 
     print(" ** Start training...")
-    for epoch in trange(epochs):
+    for epoch in trange(init_epoch, epochs, initial=init_epoch, total=epochs):
         for data, target in dataloader:
             optimizer.zero_grad()
             data, target = data.to(device), target.to(device)
@@ -34,5 +45,11 @@ if __name__ == '__main__':
             optimizer.step()
         if (epoch + 1) % 10 == 0:
             print(f" ** Epoch {epoch + 1}: {loss_value.item()}")
+        if (epoch + 1) % 100 == 0:
+            model_path = f"models/logs/unet_model_{epoch + 1}.pth"
+            torch.save(model.state_dict(), model_path)
+            print(f" ** Model saved to {model_path}")
     print(" ** Training finished.")
-    torch.save(model.state_dict(), 'model.pth')
+    model_path = "models/logs/unet_model_final.pth"
+    torch.save(model.state_dict(), model_path)
+    print(f" ** Model saved to {model_path}")
--- utils/pre_process.py
+++ utils/pre_process.py
@@ -1,3 +1,22 @@
-def pre_process(img):
-    # some pre-processing code
-    return img
+import cv2
+import numpy as np
+
+
+def color_normalization(test_image):
+    ref_image = cv2.imread("utils/ref_img.jpg")
+    test_image = test_image.astype(np.float32)
+    test_image = test_image.copy()
+    ref_image = ref_image.astype(np.float32)
+
+    test_mean, test_std = cv2.meanStdDev(test_image)
+    ref_mean, ref_std = cv2.meanStdDev(ref_image)
+
+    normalized_image = np.zeros_like(test_image)
+    for i in range(3):
+        normalized_image[:, :, i] = (
+            (test_image[:, :, i] - test_mean[i]) / test_std[i]
+        ) * ref_std[i] + ref_mean[i]
+
+    normalized_image = np.clip(normalized_image, 0, 255).astype(np.uint8)
+
+    return normalized_image
